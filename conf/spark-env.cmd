a0d2805bd8345de2da21dcaad963c10e16ab17a3
SathyaSantosh77 <kompellasantosh@gmail.com>
2024-12-01 13:14:39 -0500

    Initial Commit

---
+++ conf/spark-env.cmd
@@ -0,0 +1,10 @@
+set SPARK_MASTER_HOST=10.0.0.225
+set JAVA_HOME=C:\Users\saivi\jdk1.8.0_431
+set SPARK_WORKER_MEMORY=12g
+set SPARK_WORKER_CORES=12
+set PYSPARK_PYTHON=C:\Users\saivi\AppData\Local\Programs\Python\Python310\python.exe
+
+
+
+
+
